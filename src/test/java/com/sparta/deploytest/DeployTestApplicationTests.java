5d227068ce968ef230266d86429168326bd2bff7
WE-DA-pluggg <79959576+WE-DA-pluggg@users.noreply.github.com>
2022-07-22 09:58:18 +0900

    fd

--- src/test/java/com/sparta/deploytest/DeployTestApplicationTests.java
+++ src/test/java/com/sparta/deploytest/DeployTestApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sparta.deploytest;
+/*package com.sparta.deploytest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class DeployTestApplicationTests {
     void contextLoads() {
     }
 
-}
+}*/
